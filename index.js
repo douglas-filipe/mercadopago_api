d0bab9a9230101b5661cf9bee890b8bb3de916f4
Douglas Filipe <douglasfelipe.net77@gmail.com>
2021-12-21 11:15:27 -0400

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mercadopago = require("mercadopago");
+const cors = require("cors")
 
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json())
+
+app.use(cors())
 
 mercadopago.configure({
   sandbox: true, //Desenvolvimento
