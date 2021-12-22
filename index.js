19af7ae0a67aaf6ebcc464ce99aa4631a9a9a356
Douglas Filipe <douglasfelipe.net77@gmail.com>
2021-12-22 13:42:55 -0400

    novo

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const mercadopago = require("mercadopago");
-const cors = require("cors")
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
 mercadopago.configure({
   sandbox: true, //Desenvolvimento
-  access_token: process.env.TOKEN,
+  access_token:
+    "TEST-8703045907930339-122023-37b95e86c013f2bf5c3913e7d4e79ebd-262119893",
 });
 
 app.get("/", (req, res) => {
@@ -38,7 +39,6 @@ app.get("/pagar", async (req, res) => {
   };
   try {
     const pagamento = await mercadopago.preferences.create(dados);
-    console.log(pagamento);
 
     return res.redirect(pagamento.body.init_point);
   } catch (error) {
@@ -47,9 +47,26 @@ app.get("/pagar", async (req, res) => {
 });
 
 app.post("/not", (req, res) => {
-  console.log(req.query)
-  res.send("Ok")
-})
+  const id = req.query.id;
+  setTimeout(() => {
+    const filter = {
+      "order.id": id,
+    };
+
+    mercadopago.payment
+      .search({
+        qs: filter,
+      })
+      .then((data) => {
+        data.body.results[0];
+        console.log("data");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, 20000);
+  res.send("Ok");
+});
 
 app.listen(process.env.PORT || 8080, (req, res) => {
   console.log("App is running");
